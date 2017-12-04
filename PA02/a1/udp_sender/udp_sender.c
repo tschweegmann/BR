8c6a031c4c193dbf5cdf0f98afa42a5ba3b0e532
tschweegmann <tschweegmann@uos.de>
2017-12-04 20:43:04 +0100

    add ...

--- PA02/a1/udp_sender/udp_sender.c
+++ PA02/a1/udp_sender/udp_sender.c
@@ -2,26 +2,35 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <arpa/inet.h>
-#include <uinstd.h>
+#include <unistd.h>
 #include "Aufgabe2.h"
 
-int main(int argc, char** argv[])
+int main(int argc, char** argv)
 {
     char* adress;
     int port;
     int fd;
     struct sockaddr_in from;
     struct sockaddr_in to;
-
+    void* request = malloc(8);
     adress = argv[1];
     port = atoi(argv[2]);
-
+    int tolen;
+    unsigned short namelength
+    string filename;
+    
     //socket()
-
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    tolen = sizeof(to);
     // REQUEST_T empfangen
+    recvfrom(fd, request, sizeof(request), 0, (struct sockaddr*)&to, &tolen);
+    if (request == REQUEST_T)
+    {
+        cout << "received Request!" << endl;
+    }
 
     // HEADER_T senden (HEADER_T, unsigned short namelength, string filename, unsigned integer filesize)
-
+    sendto(fd, );
     // DATA_T senden (DATA_T, unsigned integer SeqNmbr(0-n), Daten)
 
     // SHA512_T senden (SHA512_T, SHA-512-HAshwert(64Bytes))
